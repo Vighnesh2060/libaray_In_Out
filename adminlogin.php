a23d06b39115e16d3019ecd482d09f764f222416
Vighnesh2060 <150518463+Vighnesh2060@users.noreply.github.com>
2024-02-28 09:49:04 +0530

    Update adminlogin.php

--- adminlogin.php
+++ adminlogin.php
@@ -1,19 +1,27 @@
-<?php
+<?php 
+include 'conn.php';
 // Database connection details
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "libarayinout";
+// $servername = "localhost";
+// $username = "root";
+// $password = "";
+// $dbname = "libarayinout";
 
-// Create a new MySQLi connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+// // Create a new MySQLi connection
+// $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+// // Check the connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
 // Retrieve user input from the form (make sure to sanitize and validate)
+$sql_copy_data = "INSERT INTO finaloutput (entry_date, entry_time, STATUS, name, branch, year, reason, quit_time, exit_status)
+                  SELECT e.entry_date AS entry_date, e.entry_time AS entry_time, e.STATUS AS STATUS, e.name AS name, e.branch AS branch, e.year AS year, e.reason AS reason, q.quit_time AS quit_time, q.STATUS AS exit_status
+                  FROM entry e
+                  JOIN quit q ON e.name = q.name";
+
+$conn->query($sql_copy_data);
+
 $email = $_POST['email'];
 $password = $_POST['password'];
 
